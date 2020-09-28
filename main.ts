38ad91ccc528f5a1f34fcaec1aa259fb33b979df
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-09-28 13:45:16 -0400

    Final commit

--- main.ts
+++ main.ts
@@ -7,8 +7,10 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     Counter += -1
 })
+// Created By: Vaughn Hatherall
+// 
+// Created on: September 28
+// 
+// This program counts
 let Counter = 0
 Counter = 0
-basic.forever(function () {
-	
-})
